73f760609a4b09a0e46a0db35d5b5f3df52450fd
TorgovetsSKotom <provotorov@interpolyaris.ru>
2023-07-10 09:53:13 +0300

    Last commit

--- config.def.h
+++ config.def.h
@@ -65,5 +65,10 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+	{ cpu_perc, "[CPU %s%] ", NULL },
+	{ ram_perc, "[RAM %s% ]", NULL },
+	{ ram_used, "[%s] ", NULL },
+	{ vol_perc, "[VOL %s%] ", "/dev/mixer" },
+	{ keymap, "[%s]", NULL},
+	{ datetime, "[%s]", "%F %r" },
 };
